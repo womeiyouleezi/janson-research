6221166568b5f9a5c37cbd1a3477e6406ad203a2
Junu Lee <43155891+womeiyouleezi@users.noreply.github.com>
2020-04-18 21:16:28 -0500

    made changes to gprm, phased out near function, added ability to query multiple r_n

--- .ipynb_checkpoints/gPRM_utils-checkpoint.py
+++ .ipynb_checkpoints/gPRM_utils-checkpoint.py
@@ -13,7 +13,7 @@ def dijkstra(edges, init, goal):
     for l,r,c in edges:
         g[l].append((c,r))
 
-    # dist records the min value of each node in heap.
+    # dist records the min value of each node in heap
     q, seen, dist = [(0,init,())], set(), {init: 0}
     while q:
         (cost,v1,path) = heappop(q)
@@ -26,7 +26,7 @@ def dijkstra(edges, init, goal):
         for c, v2 in g.get(v1, ()):
             if v2 in seen: continue
 
-            # Not every edge will be calculated. The edge which can improve the value of node in heap will be useful.
+            # not every edge will be calculated. The edge which can improve the value of node in heap will be useful
             if v2 not in dist or cost+c < dist[v2]:
                 dist[v2] = cost+c
                 heappush(q, (cost+c, v2, path))
@@ -79,9 +79,9 @@ def SampleFree(n, d, x_init, x_goal, distr=None):
 # Uses Dijkstra's algorithm with the edges created from Near
 def ShortestPath(edges, init, goal):
     output = dijkstra(edges, init, goal)
-    #assert not isinstance(output, float)
+    # assert not isinstance(output, float)
     try:
-        return output[0]
+        return output
     except:
         if isinstance(output, float):
             print("Shortest path does not exist")
--- gPRM.py
+++ gPRM.py
@@ -12,37 +12,65 @@ from gPRM_utils import SampleFree, Near, ShortestPath
 # Given relevant parameters, creates a class that can run gPRM over and 
 # over, randomizing the V every time.
 class gPRM:
-    def __init__(self, n, D, r, x_init, x_goal):
+    def __init__(self, n, D, x_init, x_goal):
         self.n = n
         self.D = D
-        
-        self.r = r
-        self.r_is_function = callable(r)    # True if r is a function taking in n, D
         
         self.x_init = x_init
         self.x_goal = x_goal
         self.true_distance = np.linalg.norm(x_init - x_goal)
         
         self.V = None
+        self.KD_array = []
         self.E = None
+        
         self.length = 0
         self.path = None
         
-    def run_iteration(self):
+    # Gets sample V from free space and constructs two KD trees for querying    
+    def sample_points(self):
         
         # clear memory
         del(self.V)
-        del(self.E)
+        del(self.KD_array)
         
         self.V = SampleFree(self.n, self.D, self.x_init, self.x_goal)
-        if self.r_is_function:
-            self.E = Near(self.V, self.r(self.n, self.D))
-        else:
-            self.E = Near(self.V, self.r)
-        self.length = ShortestPath(self.E, 0, 1+self.n)
         
+        # construct KD trees
+        KDT = KDTree(data=self.V)
+        search_against = KDTree(data=self.V)
+        
+        self.KD_array = [KDT, search_against]
+        
+    # With constructed KD trees, query for a given radius, construct edge set,
+    # and run a shortest path algorithm
+    def run_simulation(self, r_n):
+        
+        # clear memory
+        del(self.E)
+        
+        # run query_ball_tree
+        results = self.KD_array[0].query_ball_tree(other=self.KD_array[1], r=r_n)
+
+        # construct edge set
+        self.E = []
+        for i in range(len(self.V)):
+            for j in results[i]:
+                if i == j:
+                    continue
+                # here, i and j are indices in V
+                distance = np.linalg.norm(self.V[i] - self.V[j], ord=None)
+                (self.E).append((i, j, distance))
+        
+        # shortest path algorithm
+        self.length, self.path = ShortestPath(self.E, 0, 1+self.n)
+        
+    # The following are retrieval methods    
     def get_V(self):
         return self.V
+    
+    def get_KDTrees(self):
+        return self.KD_array
     
     def get_E(self):
         return self.E
@@ -52,6 +80,9 @@ class gPRM:
     
     def get_error(self):
         return np.abs(self.length - self.true_distance)
+    
+    def get_path(self):
+        return self.path
         
     
     
