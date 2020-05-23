8416b3a7a8c376d3fb44506eb016db96bfc41b9a
Junu Lee <43155891+womeiyouleezi@users.noreply.github.com>
2020-05-22 23:16:54 -0500

    more tests

---
+++ slurm scripts/test_job2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -t 0-00:20
+#SBATCH -p serial_requeue 		#or could be shared
+#SBATCH --mem=200
+#SBATCH -o my_test_output_%j-1.out
+#SBATCH -e my_test_errors_%j-1.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=junulee@college.harvard.edu
+#SBATCH --open-mode=append
+i=0
+seed=1000
+module load Anaconda3/5.0.1-fasrc02
+source activate junulee_env1
+while [ “$i” -lt 5 ]; do
+	python ../gPRM_simulation_junulee.py 2 1000 1 $seed
+	i=`expr $i + 1`
+	seed=`expr $seed + 1000`
