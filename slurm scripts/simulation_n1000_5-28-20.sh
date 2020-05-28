d2d223ab502bcbc576287831f9cd9cbb8a86cc50
Junu Lee <43155891+womeiyouleezi@users.noreply.github.com>
2020-05-28 16:10:43 -0500

    100 is too small

--- slurm scripts/simulation_n1000_5-28-20.sh
+++ slurm scripts/simulation_n1000_5-28-20.sh
@@ -4,8 +4,8 @@
 #SBATCH -t 0-03:00
 #SBATCH -p serial_requeue               # or could be shared
 #SBATCH --mem=2000
-#SBATCH -o data/dim2_n100.out                # use %j for jobid
-#SBATCH -e data/dim2_n100.err
+#SBATCH -o data/dim2_n1000.out                # use %j for jobid
+#SBATCH -e data/dim2_n1000.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=junulee@college.harvard.edu
 #SBATCH --open-mode=append
@@ -13,6 +13,6 @@ i=0
 module load Anaconda3/5.0.1-fasrc02
 source activate junulee_env1
 while [ "$i" -lt "5" ]; do
-        python ../gPRM_script-1.py 2 100 10 $i
+        python ../gPRM_script-1.py 2 1000 10 $i
         i=`expr $i + 1`
 done
