5de293883306264982df55db09d7c131684267d4
Junu Lee <43155891+womeiyouleezi@users.noreply.github.com>
2020-05-29 16:56:39 -0500

    new script for path histogram

---
+++ slurm scripts/simulation_n10000_5-29-20.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -t 0-18:00
+#SBATCH -p serial_requeue               # or could be shared
+#SBATCH --mem=3500
+#SBATCH -o data/5-29-20/path_data-dim2_n10000.out                # use %j for jobid
+#SBATCH -e data/5-29-20/path_data-dim2_n10000.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=junulee@college.harvard.edu
+#SBATCH --open-mode=append
+i=1
+module load Anaconda3/5.0.1-fasrc02
+source activate junulee_env1
+while [ "$i" -lt "51" ]; do
+        python ../gPRM_script-2.py 2 10000 10 $i
+        i=`expr $i + 1`
+done
