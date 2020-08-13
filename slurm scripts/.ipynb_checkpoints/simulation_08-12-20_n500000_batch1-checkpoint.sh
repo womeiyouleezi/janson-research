6df1ac5ff63ce7eb56c48f2c1f60771f24f522d4
Junu Lee <43155891+womeiyouleezi@users.noreply.github.com>
2020-08-12 21:06:25 -0500

    added scripts (10)

---
+++ slurm scripts/.ipynb_checkpoints/simulation_08-12-20_n500000_batch1-checkpoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -t 0-18:00
+#SBATCH -p serial_requeue               # or could be shared
+#SBATCH --mem=9000
+#SBATCH -o data/07-06-20/asymptotics-dim2_n500000-1.out                # use %j for jobid
+#SBATCH -e data/07-06-20/asymptotics-dim2_n500000-1.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=junulee@college.harvard.edu
+#SBATCH --open-mode=append
+i=1
+module load Anaconda3/5.0.1-fasrc02
+source activate junulee_env1
+while [ "$i" -lt "101" ]; do
+        python ../asymptotics-3.py 2 500000 $i
+        i=`expr $i + 1`
+done
+
+# this is the first batch of 100 for D=2, n=500000
