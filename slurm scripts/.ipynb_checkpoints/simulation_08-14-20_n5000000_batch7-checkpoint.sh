c2263ab353bbbc2794985d02e093fc7ceaefd94a
Junu Lee <43155891+womeiyouleezi@users.noreply.github.com>
2020-08-13 21:29:18 -0500

    one more simul, altered asymp

---
+++ slurm scripts/.ipynb_checkpoints/simulation_08-14-20_n5000000_batch7-checkpoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -t 0-24:00
+#SBATCH -p serial_requeue               # or could be shared
+#SBATCH --mem=10000
+#SBATCH -o data/08-14-20/asymptotics-dim2_n5000000-7.out                # use %j for jobid
+#SBATCH -e data/08-14-20/asymptotics-dim2_n5000000-7.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=junulee@college.harvard.edu
+#SBATCH --open-mode=append
+i=601
+module load Anaconda3/5.0.1-fasrc02
+source activate junulee_env1
+while [ "$i" -lt "701" ]; do
+        python ../asymptotics-4.py 2 5000000 $i
+        i=`expr $i + 1`
+done
+
+# this is the 7th batch of 100 for D=2, n=5000000
